753c611df39046b6772aa19927cdc1f59cd55f55
Clay Haapala <clay@haapi.mn.org>
2021-01-24 21:15:49 -0600

    Day 11 Part 2

--- day11/src/seating.clj
+++ day11/src/seating.clj
@@ -26,16 +26,20 @@
   [area [x y]]
   (= "." (get (get area x []) y ".")))
 
-(defn seat-empty?
-  "If x or y out of range, return floor (true)"
+(defn wall?
   [area [x y]]
-  (not (= "#" (get (get area x []) y ".")))
+  (= "w" (get (get area x []) y "w")))
+
+(defn seat-empty?
+  "If x or y out of range, return wall (true)"
+  [area [x y]]
+  (not (= "#" (get (get area x []) y "w")))
   )
 
 (defn occupied?
-  "if x or y out of range return floor (false)"
+  "if x or y out of range return wall (false)"
   [area [x y]]
-  (= "#" (get (get area x []) y "."))
+  (= "#" (get (get area x []) y "w"))
   )
 
 
@@ -46,6 +50,28 @@
 (defn adj-coords
   [[x y]]
   (mapv (fn [[u v]] [(+ x u) (+ y v)]) king-moves))
+
+(defn find-seat
+  [area [x y] [k m]]
+  (loop [[u v] [(+ x k) (+ y m)]]
+    (if (wall? area [u v])
+      []
+      (if (not (floor? area [u v]))
+        [u v]
+        (recur [(+ u k) (+ v m)])))))
+
+(defn look-further
+  [area x y]
+  (let [adj (count (filter #(occupied? area %)
+                           (mapv #(find-seat area [x y] %) king-moves)))
+        seat (get (get area x) y)]
+    (if (floor? area [x y])
+      seat
+      (if (and (occupied? area [x y]) (>= adj 5))
+        "L"
+        (if (and (seat-empty? area [x y]) (= adj 0))
+          "#"
+          seat)))))
 
 (defn look
   "Apply the rules to the seat, return L or #"
@@ -83,6 +109,23 @@
                newcnt
                (inc iters))))))
 
+(defn solvep2
+  [area]
+  (loop [a area scnt -1 iters 0]
+    (let [newcnt (seat-count a)
+                                        ;_ (clojure.pprint/pprint a)
+          ]
+      (if (= newcnt scnt)
+        (do
+          (println "iters:" iters)
+          newcnt)
+        (recur (mapv (fn [row]
+                       (mapv #(look-further a row %)
+                             (range (count (first area)))))
+                     (range (count area)))
+               newcnt
+               (inc iters))))))
+
 (defn -main
   [& opts]
   (let [source (if (nil? opts) "puzzleinput.txt" (first opts))
@@ -91,4 +134,4 @@
         _ (println "Counted " rowcnt "rows")
         ]
     (println "Part 1:" (solve tape))
-    (println "Part 2:" )))
+    (println "Part 2:" (solvep2 tape))))
