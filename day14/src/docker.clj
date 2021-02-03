476dc65a3f1888f9cbecb1c37e29fb0065c8b2b1
Clay Haapala <clay@haapi.mn.org>
2021-02-03 13:16:18 -0600

    Day 14 P2 getting close

--- day14/src/docker.clj
+++ day14/src/docker.clj
@@ -18,7 +18,7 @@
       (println "Read" (count lines) "lines")
       (mapv #(st/split % #" = ") lines))))
 
-(defn mask-val
+(defn mask-val-p1
   [mask val]
   ;(println mask val)
   (reduce (fn [nval b]
@@ -35,15 +35,15 @@
                   ]
               newval)) val (range (count mask))))
 
-(defn store
+(defn store-p1
   "[mem[1122] 4234] 111010XX11110X001110010XXXX10X110010"
   [[locstr sval] mask]
   (let [loc (Integer/parseInt (second (st/split locstr #"[\[\]]")))
         val (Integer/parseInt sval)
-        mval (mask-val mask val)]
+        mval (mask-val-p1 mask val)]
     [loc mval]))
 
-(defn compute
+(defn compute-p1
   [tape]
   (loop [mem {} mask 0 t tape]
     (if (empty? t)
@@ -53,18 +53,71 @@
           (cond
             (= "mask" (first inst)) (recur mem (second inst) (rest t))
             :else
-            (let [[l v] (store inst mask)]
+            (let [[l v] (store-p1 inst mask)]
               (recur (assoc mem l v) mask (rest t))))
           ))))
 
 (defn solve
   [tape]
-  (let [mem (compute tape)]
+  (let [mem (compute-p1 tape)]
     (reduce + (vals mem)))
   )
 
+(defn pow-b
+  [b]
+  (fn [y] (reduce * (take y (repeat b)))))
+
+
+
+
+(defn store
+  [[locstr sval] mask]
+  (let [loc (Integer/parseInt (second (st/split locstr #"[\[\]]")))
+        val (Integer/parseInt sval)
+        pow-2-fn (pow-b 2)
+        xoffsets (filterv #(= \X (get mask (- 35 %))) (-> mask count range))
+        bits (range (pow-2-fn (count xoffsets)))
+                                        ; locz will end up with all
+                                        ; '1' affected bits set to 1,
+                                        ; and all X floating bits set
+                                        ; to 0 (as (first bits) is always 0
+        locz (reduce (fn [l b]
+                       (let [mask-b (get mask (- 35 b))]
+                         (cond
+                           (= \1 mask-b) (bit-set l b)
+                           (= \0 mask-b) l
+                           (= \X mask-b) (bit-clear l b)))
+                       ) (-> mask count range))
+        _ (println "Xoffsets:" xoffsets)
+        ;_ (println "bits:" bits)
+        _ (println "locz:" locz)
+        _ (println "mask:" mask)
+        ]
+    (mapv (fn [nbin]
+            (let [loc (reduce (fn [l i]
+                                (let [_ (println "nbin:" nbin "bit offset:" i)
+                                      b (bit-test nbin i)]
+                                  (if b
+                                    (bit-set l (get xoffsets i))
+                                    l))) locz (range (count xoffsets)))]
+              [loc val])
+            ) bits)))
+
+(defn compute
+  [tape]
+  (loop [mem {} mask 0 t tape]
+    (if (empty? t)
+      mem
+      (let [inst (first t)]
+        (cond
+          (= "mask" (first inst)) (recur mem (second inst) (rest t))
+          :else
+          (let [kvs (store inst mask)]
+            (recur (apply (partial assoc mem) kvs) mask (rest t))))))))
+
 (defn solvep2
-  [tape])
+  [tape]
+  (reduce + (vals (compute tape))))
 
 (defn -main
   [& opts]
